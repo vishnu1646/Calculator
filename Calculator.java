67a43b6eafc03f634a6425cf3857dba50febbbf8
vishnu1646 <vishnulance98@gmail.com>
2022-11-21 11:52:07 +0530

    Cal

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+    public class Calculator {
+        public static void main(String[] args) {
+            int a,b,c;
+            int choice;
+            Scanner sc = new Scanner(System.in);
+            while(true) {
+                System.out.println("press 1 for addition");
+                System.out.println("press 2 for addition");
+                System.out.println("press 3 for multiplication");
+                System.out.println("press 4 for division");
+                System.out.println("make your choice");
+                choice = sc.nextInt();
+
+            }
+        }
+    }
+
